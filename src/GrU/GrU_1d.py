fd178c9cb5d56505336325e177967cebca718151
Thomas KIENTZ <60552083+thomktz@users.noreply.github.com>
2022-09-30 16:09:30 +0200

    added index

--- src/GrU/GrU_1d.py
+++ src/GrU/GrU_1d.py
@@ -1,4 +1,12 @@
+import plotly.graph_objects as go
+
 class GrU_1d:
-    def __init__(self, array, label):
+    def __init__(self, array, label, title, index):
         self.array = array
         self.label = label
+        self.title = title
+        self.index = [i for i in range(len(array))] if index is None else index
+    
+    def line(self):
+        self.fig = go.Figure()
+        
--- src/GrU/GrU_2d.py
+++ src/GrU/GrU_2d.py
@@ -1,4 +1,4 @@
 class GrU_2d:
-    def __init__(self, array, labels):
+    def __init__(self, array, labels, title, index):
         self.array = array
         self.labels = [i for i in range(array.shape[0])] if labels is None else labels
