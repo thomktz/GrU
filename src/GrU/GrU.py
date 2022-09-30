f575b67d9cb76313b86f99b7165d27d9422fd0fa
Thomas KIENTZ <60552083+thomktz@users.noreply.github.com>
2022-09-30 16:26:19 +0200

    1d line

--- src/GrU/GrU.py
+++ src/GrU/GrU.py
@@ -1,11 +1,11 @@
 #%%
 import numpy as np
 import pandas as pd
-from collections import Iterable
+from collections.abc import Iterable
 from GrU_1d import GrU_1d
 from GrU_2d import GrU_2d
 from GrU_3d import GrU_3d
-
+#%%
 
 class GrU:
     """
@@ -18,11 +18,11 @@ class GrU:
     - 'index' must be 1d (for now)
     """
     
-    def __new__(self, object, labels=None, title=None, index=None):
+    def __new__(self, object, labels=None, index=None):
         # Numpy arrays
         if isinstance(object, np.ndarray):
             shape = object.shape
-            if len(index) != shape[1]:
+            if index is not None and len(index) != shape[1]:
                 raise ValueError("Index must be thge same length as the array")
             if len(shape) == 1:
                 if shape[0] == 0:
@@ -30,23 +30,23 @@ class GrU:
                 if isinstance(labels, Iterable) and len(labels) != 1:
                     raise ValueError(f"Labels is of length {len(labels)}, while array is of shape (1, {shape[0]}).")
                 if labels is None:
-                    return GrU_1d(object, "0", title, index)
+                    return GrU_1d(object, "0", index)
                 elif isinstance(labels, Iterable):
-                    return GrU_1d(object, labels[0], title, index)
+                    return GrU_1d(object, labels[0], index)
                 # If not iterable:
-                return GrU_1d(object, str(labels), title, index)
+                return GrU_1d(object, str(labels), index)
 
             elif len(shape) == 2:
                 if isinstance(labels, Iterable) and len(labels) != shape[0]:
                     raise ValueError(f"Length of labels ({len(labels)}) should be the same size as the number of rows ({shape[0]}), or be None.")
                 if labels is not None:
                     raise ValueError(f"Labels should either be None, or the correct size ({shape[0]} instead of {len(labels)})")
-                return GrU_2d(object, labels, title, index)
+                return GrU_2d(object, labels, index)
             
             elif len(shape) == 3:
                 if isinstance(labels, np.ndarray) and labels.shape != shape[:2]:
                     raise ValueError(f"Shape of labels ({labels.shape}) should be the same as the of rows (shape[:2]), or be None.")
-                return GrU_3d(object, labels, title)
+                return GrU_3d(object, labels)
 
             else:
                 raise ValueError(f"Dimension of array should be 1, 2 or 3. Found {len(shape)}")
@@ -58,7 +58,7 @@ class GrU:
                     raise ValueError(f"Labels should either be None, or the correct size (1 instead of {len(labels)})")
             if labels is None:
                 return GrU_1d(object.values, "0")
-            return GrU_1d(object.values, labels, title, object.index)
+            return GrU_1d(object.values, labels, object.index)
             
         # Pandas DataFrames
         elif isinstance(object, pd.DataFrame):
@@ -67,7 +67,7 @@ class GrU:
 
         # Builtin lists
         elif isinstance(object, list):
-            return GrU(np.array(object), labels, title)
+            return GrU(np.array(object), labels)
 
         else:
             raise ValueError(f"Type {type(object)} is not accepted.")
--- src/GrU/GrU_1d.py
+++ src/GrU/GrU_1d.py
@@ -1,12 +1,22 @@
+#%%
 import plotly.graph_objects as go
 
 class GrU_1d:
-    def __init__(self, array, label, title, index):
+    def __init__(self, array, label, index):
         self.array = array
         self.label = label
-        self.title = title
         self.index = [i for i in range(len(array))] if index is None else index
     
-    def line(self):
+    def line(self, title=None):
         self.fig = go.Figure()
-        
+        self.fig.add_trace(
+            go.Scatter(
+                x=self.index, 
+                y=self.array,
+                mode='markers+lines',
+                name=self.label,
+            )
+        )
+        self.fig.update_layout(title=title)
+        return self.fig
+# %%
